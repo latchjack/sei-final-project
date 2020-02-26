8e7a8426664512555f44a72aaee8a804ddf1c363
Latch Jack <latchlernj@gmail.com>
2020-02-26 16:31:43 +0000

    user article setup

---
+++ articles/models.py
@@ -0,0 +1,23 @@
+# pylint: disable=no-member
+from django.db import models
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+# Create your models here.
+class Article(models.Model):
+  title = models.CharField(max_length=50)
+  # user = models.OneToOneRel()
+  date = models.CharField(max_length=10)
+  text = models.CharField(max_length=1000)
+  # comment = models.ManyToOneRel()
+
+  def __str__(self):
+    return self.title
+
+class Comment(models.Model):
+  text = models.CharField(max_length=140)
+  owner = models.ForeignKey(User, related_name='comments', null=True, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f'Comment by {self.owner} / {self.id} on {self.article}' # should either be article or title.
