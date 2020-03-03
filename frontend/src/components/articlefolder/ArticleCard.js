d3014ab8f59f49d91d8cbcb2e2a305fc79e3800a
Latch Jack <latchlernj@gmail.com>
2020-03-03 14:12:24 +0000

    show

--- frontend/src/components/articlefolder/ArticleCard.js
+++ frontend/src/components/articlefolder/ArticleCard.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom'
 
 const ArticleCard = ({ title, id, owner, categories }) => (
   <div key={id} className="column easier is-two-fifths is-offset-1 ">
-    <div className="box">
+    <div className="box article-card">
     <Link to={`/articles/${id}`}>
       <h1 className="article-card-title title is-4">{title}</h1>
       <h6 className="article-card-owner subtitle is-7">Article created by {owner}</h6>
       {/* <h6>{categories}</h6> */}
     </Link>
     
-      <hr />
+      <hr className="hori" />
     <div className="is-offset-3">
       <Link to={`/articles/${id}/edit/`}><button className="article-card-button">Edit Article</button></Link>
     </div>
